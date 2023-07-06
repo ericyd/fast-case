c0e8497b02a42b9914c856ed9ccfaa8abd8e5bf8
Eric Dauenhauer <eric@ericyd.com>
2023-07-06 12:24:45 -0500

    fix TS build

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,9 @@
-import { add } from "../pkg/index.js";
+import { convert_case } from "../pkg/index.js";
 
-console.log(add(3, 5));
+type Case = "camelCase" | "snake_case";
+export function convertCase(newCase: Case, value: string): string {
+  return convert_case(newCase, value);
+}
+
+export const toSnakeCase = (value: string) => convertCase("snake_case", value);
+export const toCamelCase = (value: string) => convertCase("camelCase", value);
