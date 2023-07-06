0a379d313b7da13cb129c619464bc6af5961a99c
Eric Dauenhauer <eric@ericyd.com>
2023-07-06 12:42:46 -0500

    add additional cases to TS lib

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,26 @@
 import { convert_case } from "../pkg/index.js";
 
-type Case = "camelCase" | "snake_case";
+type Case =
+  | "camelCase"
+  | "snake_case"
+  | "Sentence case"
+  | "Title Case"
+  | "PascalCase"
+  | "kebab-case";
+
 export function convertCase(newCase: Case, value: string): string {
   return convert_case(newCase, value);
 }
 
 export const toSnakeCase = (value: string) => convertCase("snake_case", value);
+
 export const toCamelCase = (value: string) => convertCase("camelCase", value);
+
+export const toSentenceCase = (value: string) =>
+  convertCase("Sentence case", value);
+
+export const toTitleCase = (value: string) => convertCase("Title Case", value);
+
+export const toPascalCase = (value: string) => convertCase("PascalCase", value);
+
+export const toKebabCase = (value: string) => convertCase("kebab-case", value);
