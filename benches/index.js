b380f8c8ed58b7348bfc6233baf25b4e65ab459b
Eric Dauenhauer <eric@ericyd.com>
2023-07-17 12:04:00 -0500

    add short string test to benchmark

--- benches/index.js
+++ benches/index.js
@@ -24,7 +24,11 @@ const {
   snakeCase,
 } = require("change-case");
 
-const testString = "SomeWhat varied test-string WITH_MULTIPLE wordCasings";
+// the overhead of copying the string is a substantial part of the runtime for fast-case.
+// Long strings perform better with fast-case because the algorithm is faster which offsets the time to copy the string.
+// Short strings perform better with change-case because the algorithm can't make up the speed lost by copying the string
+const longString = "SomeWhat varied test-string WITH_MULTIPLE wordCasings";
+const shortString = "TestLots";
 const benchmark = new Benchmarkify("UUID Processing").printHeader();
 const suiteOptions = { cycles: 100000 };
 
@@ -63,10 +67,12 @@ const suites = [
 
 suites.forEach(({ suite, fastCase, changeCase }) => {
   suite.add("fastCase", function () {
-    fastCase(testString);
+    fastCase(longString);
+    fastCase(shortString);
   });
   suite.add("changeCase", function () {
-    changeCase(testString);
+    changeCase(longString);
+    changeCase(shortString);
   });
 });
 
