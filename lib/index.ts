f948f6f3428a8f891de052bf9b161f60db8cb0de
Eric Dauenhauer <eric@ericyd.com>
2023-07-21 11:29:04 -0500

    expose SCREAMING_SNAKE_CASE in TS

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,7 @@ import {
   to_pascal_case,
   to_sentence_case,
   to_title_case,
+  to_screaming_snake_case,
 } from "../pkg/index.js";
 
 export const toSnakeCase = (value: string) => to_snake_case(value);
@@ -13,3 +14,5 @@ export const toKebabCase = (value: string) => to_kebab_case(value);
 export const toPascalCase = (value: string) => to_pascal_case(value);
 export const toSentenceCase = (value: string) => to_sentence_case(value);
 export const toTitleCase = (value: string) => to_title_case(value);
+export const toScreamingSnakeCase = (value: string) =>
+  to_screaming_snake_case(value);
